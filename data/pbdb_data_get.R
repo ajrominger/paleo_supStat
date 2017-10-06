662304242ded13e9d2e4b3380f29efa6a7407569
ajrominger <ajrominger@gmail.com>
2017-10-05 18:28:41 -0600

    trying to deal with timeouts from api

--- data/pbdb_data_get.R
+++ data/pbdb_data_get.R
@@ -17,7 +17,18 @@ subTaxa <- function(x) {
     ## get children of large clades
     chil <- lapply(as.character(x$taxon_name[tooBig]), function(tname) {
         print(tname)
-        out <- pbdb_taxa(name = tname, vocab = 'pbdb', show = 'size', rel = 'children')
+        out <- try(pbdb_taxa(name = tname, vocab = 'pbdb', show = 'size', 
+                             rel = 'children'))
+        
+        if(class(out) == 'try-error') {
+            if(grepl('port 80' %in% attr(out, 'condition'))) {
+                Sys.sleep(10)
+                out <- pbdb_taxa(name = tname, vocab = 'pbdb', show = 'size', 
+                                 rel = 'children')
+            } else {
+                stop(attr(out, 'condition'))
+            }
+        }
         
         ## hack for a bug in paleobioDB where empty columns are not returned, see:
         ## https://github.com/ropensci/paleobioDB/issues/18
@@ -29,8 +40,14 @@ subTaxa <- function(x) {
         ## avoid circular taxonomy, an issue with, e.g. Nautilida and Nautiloidea
         ## being each other's parent
         if(any(x$parent_no %in% out$taxon_no)) {
-            browser()
+            ## take out the problem child
             out <- out[!(out$taxon_no %in% x$parent_no), ]
+            
+            ## make sure we don't keep looking
+            x$size[x$parent_no %in% out$taxon_no] <<- 1
+            
+            ## make sure not to remove the parent from `x`
+            tooBig <<- tooBig[as.character(x$taxon_name[tooBig]) != tname]
         }
         
         return(out)
@@ -54,10 +71,3 @@ foo <- pbdb_occurrences(limit = 'all', base_name = allTaxa$taxon_name[191], voca
                         show = c('phylo', 'lith', 'lithext', 'loc', 'time'))
 head(foo)
 
-
-any(grepl('Canis', foo$taxon_name))
-
-
-
-pbdb_taxa(name = 'Nautiloidea', vocab = 'pbdb', show = 'size', rel = 'children')[1, ]
-pbdb_taxa(name = 'Nautilida', vocab = 'pbdb', show = 'size', rel = 'children')[1, ]
