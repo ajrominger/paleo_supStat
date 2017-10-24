5d90c9faec301a25cca87f6ed7d9b8dfe293b568
ajrominger <ajrominger@gmail.com>
2017-10-24 14:48:25 -0600

    getting info for all stages

---
+++ data/fossilworks_tbins_intervals.R
@@ -0,0 +1,35 @@
+options(stringsAsFactors = FALSE)
+
+setwd('~/Dropbox/Research/paleo_supStat/data')
+
+coreURI <- 'http://fossilworks.org/bridge.pl?a=displayInterval&interval_no='
+
+tbinInfo <- lapply(1:1108, function(i) {
+    print(i)
+    linfo <- try(readLines(paste0(coreURI, i), n = 150))
+    
+    if('try-error' %in% class(linfo)) 
+        linfo <- try(readLines(paste0(coreURI, i), n = 150))
+    
+    if('try-error' %in% class(linfo)) {
+        thisTbin <- thisMax <- thisMin <- thisName <- NA
+    } else {
+        thisTbin <- gsub('^.*10 million year bin: |<br>.*$', '', 
+                         linfo[grep('10 million year bin', linfo)])
+        thisMax <- as.numeric(gsub('^.*Lower boundary: equal to | Ma.*$|[^0-9\\.]', '',
+                                   linfo[grep('Lower boundary: equal to', linfo)]))
+        thisMin <- as.numeric(gsub('^.*Upper boundary: equal to | Ma.*$|[^0-9\\.]', '',
+                                   linfo[grep('Upper boundary: equal to', linfo)]))
+        thisName <- gsub('^.*<p class="pageTitle">|</p>.*$', '', 
+                         linfo[grep('class="pageTitle"', linfo)])
+    }
+    
+    return(data.frame(name = thisName,
+                      tbin = ifelse(length(thisTbin) == 0, NA, thisTbin), 
+                      ma_min = ifelse(length(thisMin) == 0, NA, thisMin),
+                      ma_max = ifelse(length(thisMax) == 0, NA, thisMax)))
+})
+
+tbinInfo <- do.call(rbind, tbinInfo)
+
+write.csv(tbinInfo, 'tbins_stages.csv', row.names = FALSE)
