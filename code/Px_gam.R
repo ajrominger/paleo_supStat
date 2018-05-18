4a096dd482159d0fc651e108c0a4e714f3961699
ajrominger <ajrominger@gmail.com>
2018-05-18 16:43:42 -0600

    prettified code, will not work yet untill new names are updated in other folders

--- code/Px_gam.R
+++ code/Px_gam.R
@@ -1,28 +1,42 @@
-Px.gam <- function(x,shape,rate) {
-	scale <- 1/rate
-	n <- 2*shape
-	b0 <- scale*shape
+# pdf for P(x) with f(beat) ~ Gamma
+#' @param x diversity fluctuation value
+#' @param shape the shape parameter of the gamma distribution
+#' @param rate the rate parameter  of the gamma distribution
+
+PxGam <- function(x, shape, rate) {
+	scale <- 1 / rate
+	n <- 2 * shape
+	b0 <- scale * shape
 	
-	t1 <- gamma((n+1)/2)/gamma(n/2)
-	t2 <- sqrt(b0/(pi*n))
-	t3 <- (1 + (b0*x^2)/n)^-((n+1)/2)
+	t1 <- gamma((n+1) / 2) / gamma(n / 2)
+	t2 <- sqrt(b0 / (pi * n))
+	t3 <- (1 + (b0 * x^2) / n)^-((n + 1) / 2)
 	
-	t1*t2*t3
+	t1 * t2 * t3
 }
 
-# a little recurssion!!!!
-PPx.gam <- function(x,shape,rate,comp=TRUE) {
-	if(length(x)==1) {
-		intgral <- integrate(Px.gam,0,x,shape=shape,rate=rate)
-		if(intgral$message != "OK") print(intrgral$message)
+# cdf for P(x) with f(beat) ~ Gamma
+#' @param x diversity fluctuation value
+#' @param shape the shape parameter of the gamma distribution
+#' @param rate the rate parameter  of the gamma distribution
+#' @param comp logical, whether to compute the complement or not (`comp = TRUE` is 
+#' equivilant to `lower.tail = FALSE` for typical `p` functions [e.g. `pnorm`])
+
+PPxGam <- function(x, shape, rate, comp=TRUE) {
+	if(length(x) == 1) {
+		intgral <- integrate(PxGam, 0, x, shape = shape, rate = rate)
+		if(intgral$message != 'OK') print(intrgral$message)
 		val <- intgral$value
+		
 		if(comp) {
-			return(1 - 2*val)
+			return(1 - 2 * val)
 		} else {
-			return(2*val)
+			return(2 * val)
 		}
+		
 	} else {
-		return(sapply(x,function(X) PPx.gam(X,shape,rate,comp)))
+	    # recursive handeling for multiple `x` values
+		return(sapply(x, function(X) PPxGam(X, shape, rate, comp)))
 	}
 }
 
