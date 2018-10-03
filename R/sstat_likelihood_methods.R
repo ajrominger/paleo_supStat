9002ebf4edc60427f095d6c9d92a5b1c92d6ff7a
ajrominger <ajrominger@gmail.com>
2018-10-03 13:26:27 -0600

    clean up unneccesary code and removed `MLE.sstat` function

--- R/sstat_likelihood_methods.R
+++ R/sstat_likelihood_methods.R
@@ -6,16 +6,6 @@ sstatLL <- function(par, dat) {
     -sum(log(Px.gam(dat,par[1], par[2])))
 }
 
-# might not be needed
-# MLE.sstat <- function(x, useAll = FALSE) {
-#     if(useAll) {
-#         theseDat <- unlist(x$Px.raw)
-#     } else {
-#         theseDat <- unlist(x$Px.sub)
-#     }
-#     
-#     optim(c(0.55, 0.17), sstatLL, method = 'BFGS', hessian = TRUE, dat = theseDat)
-# }
 
 #' @description bootstrap likelihood for super stats model
 #' @param x the `sstat` object
@@ -31,9 +21,8 @@ bootMLE.sstat <- function(x, B = 1000, useAll = FALSE) {
     }
     
     boots <- replicate(B, {
-        # browser()
         subDat <- sapply(theseDat, sample, size = 1)
-        # thisMLE <- try(MLE.sstat(x, useAll), silent = TRUE)
+        
         thisMLE <- try(optim(c(0.55, 0.17), sstat.lik, method = 'BFGS', hessian = TRUE, dat = subDat),
                        silent = TRUE)
         
