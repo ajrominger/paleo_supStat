90d9352e9ea1f8e7f2faf366e55f02aa4b2defea
ajrominger <ajrominger@gmail.com>
2018-10-04 14:41:43 -0600

    removed redundant plotting code

--- R/sstatComp.R
+++ R/sstatComp.R
@@ -35,27 +35,6 @@ sstatComp <- function(grp.data,minN=15,xlab="Absolute Fluctuation",ylab="Cumulat
 	qsstat <- "stub"#function(p,lower.tail=TRUE,log.p=FALSE) sstat.dist@q(p,lower.tail=lower.tail,log.p=log.p)
 	rsstat <- "stub"#function(n) sstat.dist@r(n)
 	
-	# if(plotit) {
-	# 	cat("plotting \n")
-	# 	this.ecdf <- my.ecdf(abs(unlist(p2use)),TRUE)
-	# 	
-	# 	logPlot(this.ecdf,xlab=xlab,ylab=ylab,log="xy",type="p")
-	# 	# points(my.ecdf(abs(unlist(grp.data)),TRUE),col=hsv(0,1,0,alpha=0.4))
-	# 	
-	# 	curve(this.PPx(x),col="red",add=TRUE)
-	# 	
-	# 	# best.norm <- normLS(unlist(p2use))$par
-	# 	best.norm <- c(mean(unlist(p2use)),sd(unlist(p2use)))
-	# 	
-	# 	curve(2*pnorm(x,best.norm[1],best.norm[2],lower.tail=FALSE),col="blue",add=TRUE)
-	# 	
-	# 	if(leg) {
-	# 		legend("bottomleft",legend=c("All data",paste("Data w/ N >=",minN),"Superstatistical","Best Gaussian"),
-	# 			   pch=1,lty=1,col=c(hsv(0,1,0,alpha=0.4),"black","red","blue"),
-	# 			   lwd=c(0,0,1,1),pt.lwd=1,pt.cex=c(1,1,0,0),bty="n")
-	# 	}
-	# }
-	
 	out <- list(gam.par=f.beta.par,sspar=fuent.par,beta=1/(pk.par[,"sig"])^2,sumSq=pk.par[,"ss"],minN=minN,raw.pk=p2use,
 				Px.raw=grp.data,Px.sub=p2use,incld=these2use,Px=this.Px,PPx=this.PPx,
 				dsstat=dsstat,psstat=psstat,qsstat=qsstat,rsstat=rsstat)
