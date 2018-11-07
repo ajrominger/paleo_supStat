2417be474198ff0a76ce825fba5b0daf4cac48bd
ajrominger <ajrominger@gmail.com>
2018-11-06 17:12:57 -0700

    got diversity comparisons working

--- analysis/pbdb_divCurve.R
+++ analysis/pbdb_divCurve.R
@@ -4,6 +4,7 @@
 library(divDyn)
 
 # load and prep data
+pbdbFamDiv <- read.csv('data/pbdb_3TPub-corrected.csv', row.names = 1)
 pbdbDat <- read.csv('data/pbdb_data.csv', as.is = TRUE)
 tbin <- read.csv('data/tbinsMid.csv', as.is = TRUE)
 tbin$tbin <- factor(tbin$tbin, levels = tbin$tbin)
@@ -12,48 +13,42 @@ pbdbDat$tbinNum <- as.integer(pbdbDat$tbin)
 
 pbdbDatUnique <- pbdbDat[!duplicated(paste0(pbdbDat$collection_no, pbdbDat$otu)), ]
 
+# subsampled richness
+pbdbCR <- subsample(pbdbDatUnique, bin = 'tbinNum', tax = 'otu', iter = 10, q = 55, 
+                    type = 'cr', unit = 'reference_no')
+pbdbSQS <- subsample(pbdbDatUnique, bin = 'tbinNum', tax = 'otu', iter = 50, q = 0.75, 
+                     ref = 'reference_no', type = 'sqs', singleton = 'ref')
+
+# our new richness estimate
+pbdbT3Pub <- rowSums(pbdbFamDiv)
 
 
-data(corals)
-data(stages)
-fossils <- corals[corals$stg != 95, ]
+# plot fluctuations to see that they're comprable
+pdf('ms/figSupp_divEstComp.pdf', width = 7.5, height = 4)
+layout(matrix(1:2, nrow = 1))
 
-# indicate identical collection/genus combinations
-collGenus <- paste(fossils$collection_no, fossils$genus)
-# omit the duplicates from the occurrence datasets
-fossGen <- fossils[!duplicated(collGenus),]
+par(mar = c(4.5, 2.5, 0, 0.5) + 0.5, mgp = c(2, 0.75, 0))
+plot(1, xlim = c(540, 0), ylim = c(-500, 500), type = 'n', xaxt = 'n', 
+     xlab = '', ylab = 'Richness', xaxs = 'i')
+paleoAxis(1)
+mtext('Millions of years ago', side = 1, line = 3.5)
 
-
-# rarifaction considering collections as well as occurrences
-subUWunit <- subsample(fossGen, bin="stg", tax="genus",
-                       iter=100, q=10, type="cr", unit="collection_no")
-# classic rarifaction
-subCR <- subsample(fossGen, bin="stg", tax="genus", iter=100, q=40)
-
-# occurrence-weighted by list
-subOW <- subsample(fossGen, bin="stg", tax="genus", coll="collection_no",
-                   iter=100, q=40, type="oxw")
+lines(tbin$ma_mid[-1], diff(pbdbCR$divCSIB), col = 'black', lwd = 2)
+lines(tbin$ma_mid[-1], diff(pbdbSQS$divCSIB), col = 'blue', lwd = 2)
+lines(tbin$ma_mid[-c(1:2, nrow(tbin))], diff(pbdbT3Pub), col = 'red', lwd = 2)
 
 
+par(mar = c(3, 3, 0, 0) + 0.5, mgp = c(2, 0.75, 0))
+plot(simpECDF(abs(diff(pbdbT3Pub)), complement = TRUE), col = 'red', log = 'xy', 
+     type = 'l', lwd = 2, xlim = c(1, 500),
+     panel.first = {
+         lines(simpECDF(abs(diff(pbdbCR$divCSIB)), complement = TRUE), 
+               col = 'black', lwd = 2)
+         lines(simpECDF(abs(diff(pbdbSQS$divCSIB)), complement = TRUE), 
+               col = 'blue', lwd = 2)
+     }, 
+     axes = FALSE, frame.plot = TRUE, 
+     xlab = '|Fluctuations|', ylab = 'Cumulative density')
+logAxis(1:2)
 
-# sqs with good's u based on different singletons
-sqsCollSing <-subsample(fossGen, iter=50, q=0.4,
-                        tax="genus", bin="stg", type="sqs",
-                        singleton="occ")
-sqsRefSing <-subsample(fossGen, iter=50, q=0.4,
-                       tax="genus", bin="stg", type="sqs", ref="reference_no",
-                       singleton="ref")
-sqsNoSing <-subsample(fossGen, iter=50, q=0.4,
-                      tax="genus", bin="stg", type="sqs", singleton=FALSE)
-
-# sqs with collection correction
-sqsByColl <-subsample(fossGen, iter=50, q=0.5,
-                      tax="genus", bin="stg", ref="reference_no",coll="collection_no",
-                      type="sqs", singleton="ref", excludeDominant=T, largestColl=T, byList=TRUE)
-
-tsplot(stages, shading="series", boxes="per", xlim=51:95,
-       ylab="corrected SIB diversity", ylim=c(-3, 3.5))
-lines(stages$mid[1:94], scale(subCR$divCSIB), col="black")
-# lines(stages$mid[1:94], scale(subUWunit$divCSIB), col="blue")
-# lines(stages$mid[1:94], scale(subOW$divCSIB), col="red")
-lines(stages$mid[1:94], scale(sqsByColl$divCSIB), col="blue")
+dev.off()
