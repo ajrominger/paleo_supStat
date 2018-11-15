e6758d2b481f08ea03ca5dfed182aa132896e0cd
ajrominger <ajrominger@gmail.com>
2018-11-15 14:55:20 -0700

    deleted all the old shit

--- code/shapiro_multi_test.R
+++
@@ -1,7 +0,0 @@
-source("~/R_functions/ks_stat.R")
-
-x <- replicate(400,shapiro.test(rnorm(round(runif(1,10,100)),mean=rnorm(1,0,0.5),sd=1/sqrt(rgamma(1,2,1))))$statistic)
-
-hist(x)
-
-ks.test(x=x,y=punif)
--- pbdb/pbdb_sqs/pbdb_sstat_ordSQS.R
+++
@@ -1,7 +0,0 @@
-#########  calculate sqs for pbdb by order  #########
-
-##	depends on objects made in .../pbdb_sstat_cover.R
-source("/Users/andrewrominger/Desktop/Research/paleo_supStat/pbdb_sstat_cover.R")
-
-pbdb.ord.sqs <- lapply(pbdb.ord.tbin10[1:3,3],function(x) )
-
