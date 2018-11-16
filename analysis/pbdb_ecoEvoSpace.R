2bf961225fb7520169e14d836b70a5d471cc6100
ajrominger <ajrominger@gmail.com>
2018-11-16 14:31:49 -0700

    made the plotting pretty

--- analysis/pbdb_ecoEvoSpace.R
+++ analysis/pbdb_ecoEvoSpace.R
@@ -1,6 +1,8 @@
 # **a script to evaluate hour occupancy of eco-evolutionary space changes 
 # across taxonomy**
 
+library(socorro)
+
 pbdbDat <- read.csv('data/pbdb_data.csv', as.is = TRUE)
 
 # extract only the eco/evo/life history data and remove duplicates
@@ -31,19 +33,23 @@ eeOccBoot <- function(x, B, fun) {
 }
 
 
-
-
+# calculate eco-evolutionary space occupancy of each taxonomic level
 famEE <- eeOccBoot(eeOcc(eeSpaceDat$family, eeSpaceDat[, -(1:5)]), 500, mean)
 ordEE <- eeOccBoot(eeOcc(eeSpaceDat$order, eeSpaceDat[, -(1:5)]), 500, mean)
 clsEE <- eeOccBoot(eeOcc(eeSpaceDat$class, eeSpaceDat[, -(1:5)]), 500, mean)
 phyEE <- eeOccBoot(eeOcc(eeSpaceDat$phylum, eeSpaceDat[, -(1:5)]), 500, mean)
 
 
-plot(1:4, ylim = c(1, 100), type = 'n', log = 'y', yaxt = 'n')
+# plotting
+pdf('ms/figSupp_eeSpaceOcc.pdf', width = 4, height = 3.5)
+
+par(mar = c(3, 3, 0, 0) + 0.5, mgp = c(2, 0.75, 0))
+plot(1:4, ylim = c(1, 100), type = 'n', log = 'y', yaxt = 'n', xaxt = 'n',
+     xlab = 'Taxonomic level', ylab = 'Number of Bambach guilds')
+axis(1, at = 1:4, labels = c('Families', 'Orders', 'Classes', 'Phyla'))
 logAxis(2)
 segments(x0 = 1:4, y0 = c(min(famEE), min(ordEE), min(clsEE), min(phyEE)), 
-         y1 = c(max(famEE), max(ordEE), max(clsEE), max(phyEE)))
-points(1:4, c(mean(famEE), mean(ordEE), mean(clsEE), mean(phyEE)), pch = 16)
+         y1 = c(max(famEE), max(ordEE), max(clsEE), max(phyEE)), lwd = 2)
+points(1:4, c(mean(famEE), mean(ordEE), mean(clsEE), mean(phyEE)), pch = 16, cex = 1.2)
 
-
-
+dev.off()
