92a62decc70a4c09e7e5508f1505e5cc37055c5b
ajrominger <ajrominger@gmail.com>
2018-11-15 20:22:55 -0700

    made mle its own function

--- R/sstat_likelihood_methods.R
+++ R/sstat_likelihood_methods.R
@@ -7,10 +7,20 @@ sstatLL <- function(par, dat) {
 }
 
 
+#' @description finds the maximum likelihood estimate of the superstats model
+#' @param dat the data to fit
+
+sstatMLE <- function(dat) {
+    optim(c(0.55, 0.17), sstatLL, method = 'BFGS', hessian = TRUE, 
+          dat = dat)
+}
+
+
 #' @description bootstrap likelihood for super stats model
 #' @param x the `sstat` object
 #' @param B the number of boostrap replicates
-#' @param useAll logical, whether all orders, or only those with the minimum number of occurences as specified
+#' @param useAll logical, whether all orders, or only those with the minimum number of 
+#' occurences as specified
 #' in `make3TPub` argument `minPub` should be used
 
 bootMLE.sstat <- function(x, B = 1000, useAll = FALSE) {
@@ -23,8 +33,7 @@ bootMLE.sstat <- function(x, B = 1000, useAll = FALSE) {
     boots <- replicate(B, {
         subDat <- sapply(theseDat, sample, size = 1)
         
-        thisMLE <- try(optim(c(0.55, 0.17), sstatLL, method = 'BFGS', hessian = TRUE, dat = subDat),
-                       silent = TRUE)
+        thisMLE <- try(sstatMLE(subDat), silent = TRUE)
         
         if(class(thisMLE) !=  'try-error') {
             if(thisMLE$convergence !=  0) {
@@ -48,8 +57,10 @@ bootMLE.sstat <- function(x, B = 1000, useAll = FALSE) {
 
 #' @description logLik for sstat class
 #' @param x the `sstat` object
-#' @param fitted logical, was the model fitted by max likelihood or computed from first principles
-#' @param useAll logical, should all data be used, or only those taxa that have greater than `minN` occurrences
+#' @param fitted logical, was the model fitted by max likelihood or computed from first 
+#' principles
+#' @param useAll logical, should all data be used, or only those taxa that have greater 
+#' than `minN` occurrences
 #' as specified in `sstatComp`
 
 logLik.sstat <- function(x, fitted = TRUE, useAll = FALSE) {
