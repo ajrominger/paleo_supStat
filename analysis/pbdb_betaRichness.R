fb814391bd048cce6b630fa8016d1e674c3bf5b4
ajrominger <ajrominger@gmail.com>
2018-11-21 16:10:52 -0700

    wrote out beta by richness fig

--- analysis/pbdb_betaRichness.R
+++ analysis/pbdb_betaRichness.R
@@ -1,14 +1,14 @@
+library(socorro)
+
+load('data/pbdb_sstat_objects.RData')
 pbdbDat <- read.csv('data/pbdb_data.csv', as.is = TRUE)
-famDiv <- read.csv('data/pbdb_3TPub-corrected.csv', row.names = 1)
-
-
 
 divFamRaw <- tapply(pbdbDat$otu, pbdbDat$family, function(x) length(unique(x)))
-divFamCor <- colSums(famDiv)
-foo <- as.matrix(famDiv)
-foo[foo == 0] <- NA
 
-divFamMean <- colMeans(foo, na.rm = TRUE)
-
-plot(1/sstatPBDBfam3TP$beta, divFamCor[names(sstatPBDBfam3TP$beta)], log = 'xy')
-plot(1/sstatPBDBfam3TP$beta, divFamMean[names(sstatPBDBfam3TP$beta)], log = 'xy')
+pdf('ms/figSupp_betaByRich.pdf', width = 4, height = 4)
+par(mar = c(3, 3, 0, 0) + 0.5, mgp = c(2, 0.75, 0))
+plot(divFamRaw[names(sstatPBDBfam3TP$beta)], sstatPBDBfam3TP$beta, log = 'xy', 
+     xlab = 'Number of genera', ylab = expression(beta), 
+     axes = FALSE, frame.plot = TRUE)
+logAxis(1:2)
+dev.off()
